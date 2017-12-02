8abeb353ff8fab4ed590a7c441144eeb9258b69f
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-02 15:41:35 +0900

    user format test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-  
+
   # name, email 入力必須 最大長
   validates(:name, presence: true, length: {maximum: 50})
-  
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates(:email, presence: true, length: {maximum: 128}, format: {with: VALID_EMAIL_REGEX})
-  
-  
+  validates(:email, presence: true, length: {maximum: 128}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false})
+
+
 end
