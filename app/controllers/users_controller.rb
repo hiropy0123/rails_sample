015a0ed09ab9ec1a3a5e81705b26dcbe4663dca8
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-08 23:59:15 +0900

    登録完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:id])
+    @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render 'new'
     end
