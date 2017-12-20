51af742ffd6ab5565332b07c93f63d0dea2c2470
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-21 00:08:11 +0900

    test helper

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,7 +21,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "login with valid information followed by logout" do
   get login_path
   post login_path, params: { session: { email: @user.email, password: 'password' } }
-  # assert is_logged_in?
+  assert is_logged_in?
   assert_redirected_to @user
   follow_redirect!
   assert_template 'users/show'
@@ -29,7 +29,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   assert_select "a[href=?]", logout_path
   assert_select "a[href=?]", user_path(@user)
   delete logout_path
-  # assert_not is_logged_in?
+  assert_not is_logged_in?
   assert_redirected_to root_url
   # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
   delete logout_path
