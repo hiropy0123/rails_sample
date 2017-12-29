178aef13f1de529757e5f3d36222d3b33c18ce99
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-29 16:06:06 +0900

    10.2 フレンドリーフォワーディング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       log_in user
       # remember_meにチェックを入れたら'1' 記憶する
       params[:session][:remember_me] === '1' ? remember(user) : forget(user)
-      redirect_to user
+      # redirect_to user
+      redirect_back_or user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = "メールアドレスとパスワードの組み合わせが正しくありません。"
