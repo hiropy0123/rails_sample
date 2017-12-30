da8461c9f0901ea2c703ef2d400948fe7c1ea4dd
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-30 21:39:38 +0900

    パスワードを更新する edit アクション

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -17,4 +21,42 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+    elsif @user.update_attributes(user_params)
+      log_in @user
+      flash[:success] = "パスワードが再設定されました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    # 正しいユーザーかどうか確認する
+    def valid_user
+      unless ( @user && @user.activated? && @user.authenticated?(:reset, params[:id]) )
+        # 正しいユーザーでなかったら、TOPにリダイレクト
+        redirect_to root_url
+      end
+    end
+
+    # 期限切れかどうかを確認する
+    def check_expiration
+      if @user.password_resset_expired?
+        flash[:danger] = "このパスワードの再発行URLは期限切れです。"
+        redirect_to new_password_reset_url
+      end
+    end
+
 end
