d1b0ce7dc30da842a389fc383fe0780bc2d505c6
HirokiNakashima <hiroki123naka4ma@gmail.com>
2017-12-30 13:47:41 +0900

    host 再度修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'alexcodesign.work'
+  host = 'http://www.alexcodesign.work'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
