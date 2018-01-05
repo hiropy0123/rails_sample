2c133001079e427b28a8482779f6fe635cf193b0
HirokiNakashima <hiroki123naka4ma@gmail.com>
2018-01-05 17:42:41 +0900

    14.2.2 統計と [Follow] フォーム

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,15 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
 
   # RESTful resources
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   # rootは最後に記述する
   root 'static_pages#home'
